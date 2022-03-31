e560cf546a5bac5b48cf5f377d5d72976b137762
Francesco <92933627+Chirichett@users.noreply.github.com>
2022-03-31 14:06:58 +0200

    Initial Commit

---
+++ animation_mc/animation_mcApp.swift
@@ -0,0 +1,17 @@
+//
+//  animation_mcApp.swift
+//  animation_mc
+//
+//  Created by Francesco Pirone on 28/03/22.
+//
+
+import SwiftUI
+
+@main
+struct animation_mcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Microinteractions()
+        }
+    }
+}
